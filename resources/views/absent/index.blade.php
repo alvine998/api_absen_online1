b1522588fc2b8837ea7e623a5f4418f50298ab3c
alvin <alvinecom2018@gmail.com>
2024-04-22 17:24:12 +0700

    --

--- resources/views/absent/index.blade.php
+++ resources/views/absent/index.blade.php
@@ -54,7 +54,7 @@
                         </td>
                         <td class="border px-4 py-2 border-black flex items-center justify-center">
                         {{Storage::url('public/storage/').$absents->image}}
-                            <img src="{{Storage::url('public/storage/').$absents->image}}" alt="img-absent" class="w-[150px] h-[150px]">
+                            <!-- <img src="{{Storage::url('public/storage/').$absents->image}}" alt="img-absent" class="w-[150px] h-[150px]"> -->
                         </td>
                     </tr>
                     @empty
