b56b5bb1283fb98ba4a985b5314a94a45e2775e5
alvin <alvinecom2018@gmail.com>
2024-06-20 21:58:47 +0700

    add so_code

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -10,6 +10,7 @@ class Stock extends Model
     use HasFactory;
 
     protected $fillable = [
+        'so_code',
         'ref_no',
         'visit_id',
         'user_id',
@@ -29,5 +30,28 @@ class Stock extends Model
     public function products()
     {
         return $this->hasMany(Product::class);
+    }
+
+    public function generateSoCode()
+    {
+        $datePart = date('ymd');
+        $lastOrder = static::where('so_code', 'like', "SO{$datePart}-%")->orderBy('so_code', 'desc')->first();
+
+        if (!$lastOrder) {
+            $number = 1;
+        } else {
+            $lastNumber = (int)substr($lastOrder->so_code, 9);
+            $number = $lastNumber + 1;
+        }
+
+        return sprintf("SO%s-%04d", $datePart, $number);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->so_code = $model->generateSoCode();
+        });
     }
 }
--- database/migrations/2024_04_09_034325_create_stocks_table.php
+++ database/migrations/2024_04_09_034325_create_stocks_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('stocks', function (Blueprint $table) {
             $table->id();
+            $table->string('so_code');
             $table->integer('visit_id')->nullable();
             $table->integer('user_id')->nullable();
             $table->integer('user_name')->nullable();
